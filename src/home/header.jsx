322306e1aa2255f280d09bf32501ca1d538972db
mohoaj <mohoabja@gmail.com>
2024-06-01 15:12:21 -0700

    learn more fix

--- src/home/header.jsx
+++ src/home/header.jsx
@@ -14,7 +14,7 @@ export const Header = (props) => {
                 </h1>
                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
                 <a
-                  href="#meetyourassistants"
+                  href="#assistants"
                   className="text-center btn btn-custom btn-lg page-scroll"
                 >
                   Learn More
