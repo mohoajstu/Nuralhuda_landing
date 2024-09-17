8472634ef918464952d601b88fb107db6d11419d
mohoaj <mohoabja@gmail.com>
2024-09-17 00:20:53 -0700

    removed tools.css

--- src/home/tools.jsx
+++ src/home/tools.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './tools.css'; // Import the custom CSS file
+
 
 export const Tools = (props) => {
   const navigate = useNavigate();
